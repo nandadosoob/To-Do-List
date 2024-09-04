dc98eb1b603853f5e2dbfc46d98bc4223f6ff88f
nandaBe <130059223+nandadosoob@users.noreply.github.com>
2024-09-04 10:37:35 -0300

    calmee

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
-import { Main}  from "./components/Main/Main";
+import { Main } from "./components/Main/Main";
+import styles from "./App.module.css"
 
 export function App() {
   return (
-    <>
-      <Header/>
+    <div className={styles.App}>
+
+
+      <Header />
       <Main>
       </Main>
       <Footer autor="Fernanda" />
-    </>
+
+    </div>
+
   )
 }
 
